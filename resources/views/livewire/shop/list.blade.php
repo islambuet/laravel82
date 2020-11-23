d28d92ebcc72fdf97088a708cc3005d87fe57be6
Md. Shaiful Islam <islambuet@gmail.com>
2020-11-24 01:24:25 +0600

    list completed

--- resources/views/livewire/shop/list.blade.php
+++ resources/views/livewire/shop/list.blade.php
@@ -1,8 +1,50 @@
 <div> 
+    <style>
+        .carousel-control-next,
+        .carousel-control-prev /*, .carousel-indicators */ {
+            filter: invert(100%);
+        }
+    </style>
     <div class="row m-0"> 
         @foreach ($products as $product)
-        <div class="col-4 col-sm-3">
-            {{$product->name}}
+        <div class="col-6 col-sm-4 pb-2">
+            @if(isset($pictures[$product->id]))
+                @if(count($pictures[$product->id])>1)                            
+                    <div id="carousel_{{$product->id}}" class="carousel slide" data-ride="carousel" style="height: 300px!important;">
+                        <div class="carousel-inner">
+                            @foreach($pictures[$product->id] as $key=>$picture)
+                            <div class="carousel-item {{$key==0?'active':''}}">
+                                <img class="d-block" style="max-height: 100%; max-width:100%" src="{{ asset('storage/'.$picture['picture']) }}" alt="Product Picture">
+                            </div>                                
+                            @endforeach
+                        </div>
+                        <a class="carousel-control-prev" href="#carousel_{{$product->id}}" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carousel_{{$product->id}}" role="button" data-slide="next" >
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                @else
+                <div style="height:300px">
+                    <img class="d-block" style="max-height: 100%; max-width:100%" src="{{ asset('storage/'.$pictures[$product->id][0]['picture']) }}" alt="comming">
+                </div>                
+                @endif
+            @else 
+            <div style="height:300px">
+                <img class="d-block" style="max-height: 100%; max-width:100%" src="{{ asset('storage/products/comming.png') }}" alt="comming">
+            </div>                
+                
+            @endif
+            
+            
+            <div>{{$product->name}}</div>
+            <div>Price :{{$product->price}}</div>
+            <div><input class="btn btn-primary" type="button" value="Buy Now"></div>
+
+            
         </div>
             
         @endforeach  
