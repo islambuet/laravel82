1e6afc3bb82635d7633938e10933333099b837dc
Md. Shaiful Islam <islambuet@gmail.com>
2020-12-01 09:56:42 +0600

    Login Controller working

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,19 +2,53 @@
 
 namespace App\Http\Controllers;
 use  App\Http\Controllers\RootController;
+use Illuminate\Support\Str;
 
+use Illuminate\Support\Facades\Auth; 
 use Illuminate\Http\Request;
 
 class UserController extends RootController
 {
     function __construct()
     {
-        parent::__construct();        
+        parent::__construct(); 
+        $this->middleware('auth:api')->only(
+            ['getUser']
+        );
         
     }   
     public function initialize(Request $request)
     {
         return response()->json([
             'initalize' => "user"],201);
+    }
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
+            $user = Auth::user(); 
+            $token = Str::random(60);
+            $user->forceFill([
+                'api_token' => hash('sha256', $token),
+            ])->save();
+
+            return response()->json(['token' => $token,'user'=>$user->toArray()], 200); 
+        } 
+        else{ 
+            return response()->json(['errorStr'=>'Unauthorised'], 401); 
+        } 
+    }
+    public function getUser(Request $request)
+    {
+        
+        $user = Auth::user(); 
+        if($user)
+        {
+            return response()->json(['user'=>$user->toArray()], 200); 
+        }
+        else
+        {
+            return response()->json(['errorStr'=>'Unauthorised','message'=>"user empty"], 401); 
+        }
+        
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+//use App\Http\Controllers\UserController;
 use App\Http\Controllers\NotFoundController;
 
 /*
@@ -15,13 +15,12 @@ use App\Http\Controllers\NotFoundController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::get('/', function () {
-    return 'Hello World';
-});
-Route::get('user/initialize', [UserController::class, 'initialize']);
+// Route::middleware('auth:api')->get('user', function (Request $request) {
+//     return $request->user();
+// });
+Route::get('user/initialize', [App\Http\Controllers\UserController::class, 'initialize']);
+Route::get('user/login', [App\Http\Controllers\UserController::class,'login']);
+Route::post('user/login', [App\Http\Controllers\UserController::class,'login']);
+Route::get('user', [App\Http\Controllers\UserController::class,'getUser']);
 
 Route::fallback([NotFoundController::class,'index']);
